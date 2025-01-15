4e1342564accd2275a7bc0e9503999af6d7629af
KevinWeiss1995 <kjweiss1995@gmail.com>
2025-01-14 20:13:40 -0500

    Add phihsing scalar model and debug app

--- tests/phishing_test/phish_test.py
+++ tests/phishing_test/phish_test.py
@@ -1,17 +1,17 @@
 import requests
 
-url = "http://localhost:5001/predict/phishing"
+url = "http://localhost:5000/predict/phishing"
 sample_data = [
     [
-        1,  # having_IP_Address
-        0,  # URL_Length
-        -1, # Shortining_Service
-        1,  # having_At_Symbol
-        1,  # double_slash_redirecting
-        -1, # Prefix_Suffix
-        1,  # having_Sub_Domain
-        1,  # SSLfinal_State
-        1   # Domain_registeration_length
+    0,  # having_IP_Address (0 indicates absence of an IP address in the URL)
+    1,   # URL_Length (long URL length, which is often used in legitimate sites)
+    -1,  # Shortining_Service (0 indicates no use of a URL shortening service)
+    -1,  # having_At_Symbol (absence of "@" in the URL)
+    -1,  # double_slash_redirecting (indicates no double slash in the URL)
+    1,   # Prefix_Suffix (presence of prefix or suffix, which is common in legitimate URLs)
+    -1,  # having_Sub_Domain (absence of subdomain, often used in legitimate sites)
+    -1,  # SSLfinal_State (0 indicates SSL is not present, which is common for non-phishing)
+    1    # Domain_registeration_length (long registration length, often used in legitimate domains)
     ]
 ]
 
