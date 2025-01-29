a66ee42befd5b3d075682dacbd80478760b8ceca
Hector Alicea <alex@MacBook-Pro-2.local>
2025-01-28 23:22:01 -0400

    Fixed path resolution logic (so its more universal) :P

--- preprocessing/DDoS/combine_data.py
+++ preprocessing/DDoS/combine_data.py
@@ -1,7 +1,12 @@
 import pandas as pd
+import os
 
-benign_df = pd.read_csv("/Users/kweiss/git/cyber/ANTS/data/DDoS/BENIGN_sampled.csv")
-ddos_df = pd.read_csv("/Users/kweiss/git/cyber/ANTS/data/DDoS/DDoS.csv")
+# Get the path relative to the current script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+data_dir = os.path.abspath(os.path.join(current_dir, '..', '..', 'data', 'DDoS'))
+
+benign_df = pd.read_csv(os.path.join(data_dir, "BENIGN_sampled.csv"))
+ddos_df = pd.read_csv(os.path.join(data_dir, "DDoS.csv"))
 
 combined_df = pd.concat([benign_df, ddos_df]).sample(frac=1, random_state=42).reset_index(drop=True)
-combined_df.to_csv("/Users/kweiss/git/cyber/ANTS/data/DDoS/TRAINING_DATA.csv", index=False)
+combined_df.to_csv(os.path.join(data_dir, "TRAINING_DATA.csv"), index=False)
